d5c38f7a8d9e14de376f09b575be7d3b9ce786fd
Mirraely <mirraelymoura@gmail.com>
2023-10-29 15:16:56 -0300

    Adiciona animação ao título da home e insere link para projeto

--- js/index-animacao.js
+++ js/index-animacao.js
@@ -1,13 +1,5 @@
-// var typed = new Typed('#titulo-animacao', {
-//     strings: ['Olá, meu nome é Mirraely Moura e construo sistemas web'],
-//     loop: true,
-//     typeSpeed: 100,
-// });
-
-// $(function () {
-//     $('.container').starfield({
-//         starDensity: 1.0,
-//         mouseScale: 0.1,
-//         seedMovement: false
-//     });
-// });
+var typed = new Typed('.skill-desenvolvedor', {
+    strings: ['Desenvolvedora Frontend'],
+    loop: true,
+    typeSpeed: 100,
+});
