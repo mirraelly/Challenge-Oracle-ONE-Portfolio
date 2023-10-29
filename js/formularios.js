7bd621e98ba2819d6c0270b814a40cda8c2084a4
Mirraely <mirraelymoura@gmail.com>
2023-10-29 11:48:42 -0300

    Adiciona gif ao campo contato e ajusta js do formulário

--- js/formularios.js
+++ js/formularios.js
@@ -3,14 +3,23 @@ const camposObrigatorios = document.querySelector('.form-contato-form');
 const form = document.querySelector('.form-contato-form');
 const nome = document.querySelector('#nome');
 const email = document.querySelector('#email');
+const telefone = document.querySelector('#telefone');
 const assunto = document.querySelector('#assunto');
 const mensagem = document.querySelector('#mensagem');
 const input = document.getElementById("input");
+const radioEmail = document.querySelector('#radio-email');
+const radioTelefone = document.querySelector('#radio-telefone');
+const radioWhatsapp = document.querySelector('#radio-whatsapp');
 
-submit.addEventListener('input', validar);
+atualizaRequeridos();
 
 function validar(event) {
+    form.classList.add('validado');
     event.preventDefault();
+
+    if (!form.checkValidity()) {
+        alert('Dados Inválido!');
+    }
 
     let valido = true;
 
@@ -28,9 +37,37 @@ function validar(event) {
         email.setCustomValidity("");
     }
 
-
-
     return valido;
 
 }
 
+form.addEventListener('submit', validar);
+
+radioEmail.addEventListener('click', () => {
+    email.focus();
+    atualizaRequeridos();
+});
+
+radioTelefone.addEventListener('click', () => {
+    telefone.focus();
+    atualizaRequeridos();
+});
+
+radioWhatsapp.addEventListener('click', () => {
+    telefone.focus();
+    atualizaRequeridos();
+});
+
+function atualizaRequeridos() {
+    if (radioEmail.checked) {
+        email.setAttribute('required', true);
+    } else {
+        email.removeAttribute('required');
+    }
+
+    if (radioTelefone.checked || radioWhatsapp.checked) {
+        telefone.setAttribute('required', true);
+    } else {
+        telefone.removeAttribute('required');
+    }
+};
--- js/index-animacao.js
+++ js/index-animacao.js
@@ -1,13 +1,13 @@
-var typed = new Typed('#titulo-animacao', {
-    strings: ['Olá, meu nome é Mirraely Moura e construo sistemas web'],
-    loop: true,
-    typeSpeed: 100,
-});
+// var typed = new Typed('#titulo-animacao', {
+//     strings: ['Olá, meu nome é Mirraely Moura e construo sistemas web'],
+//     loop: true,
+//     typeSpeed: 100,
+// });
 
-$(function () {
-    $('.container').starfield({
-        starDensity: 1.0,
-        mouseScale: 0.1,
-        seedMovement: false
-    });
-});
+// $(function () {
+//     $('.container').starfield({
+//         starDensity: 1.0,
+//         mouseScale: 0.1,
+//         seedMovement: false
+//     });
+// });
