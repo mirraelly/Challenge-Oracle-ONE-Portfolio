19449237670a570b6d17e08bdb72a7768f014189
Mirraely <mirraelymoura@gmail.com>
2023-10-29 20:54:07 -0300

    Adiciona js de scroll

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,28 @@
 const header = document.querySelector('header');
 const links = document.querySelectorAll('.menu-nav ul li a');
+const sections = document.querySelectorAll("main > section");
 
 atualizarMenu();
 
 window.addEventListener('scroll', () => {
-    diminuirMenu()
+    diminuirMenu();
+
+    var scrollPosition = (document.documentElement.scrollTop || document.body.scrollTop) + 50;
+    let sectionAtual = sections[0];
+
+    for (const section of sections) {
+        if (section.offsetTop >= scrollPosition) {
+            break;
+        }
+        sectionAtual = section;
+    }
+
+    links.forEach((link) => {
+        link.classList.remove('active');
+    });
+
+    const itemMenu = document.querySelector('.menu-nav ul li a[href*="#' + sectionAtual.id + '"]');
+    itemMenu.classList.add('active');
 });
 
 function diminuirMenu() {
