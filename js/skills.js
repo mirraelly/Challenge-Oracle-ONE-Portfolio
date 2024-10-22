63ee7cc5653846bcd4eb58a24a7b03d5b4e533f0
Mirraely <mirraelymoura@gmail.com>
2024-10-22 17:47:43 -0300

    Adiciona alterações de dados

---
+++ js/skills.js
@@ -0,0 +1,10 @@
+const images = []
+
+const containerSkills = document.querySelector('.skills-section');
+containerSkills.innerHTML = flip;
+
+function flip() {
+    fetch('./json/skills.json');
+
+}
+
